d3da647b1635d7668fd6225fc11e6bc2e5013080
MMarioB <mmariob196@gmail.com>
2025-01-08 13:39:18 +0100

    fix hook

--- src/hooks/useSpotify.js
+++ src/hooks/useSpotify.js
@@ -1,47 +1,60 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SpotifyWebApi from 'spotify-web-api-js';
 import { loginUrl, getTokenFromUrl } from '../lib/spotify';
 
 const spotifyApi = new SpotifyWebApi();
 
 export const useSpotify = () => {
-    const [token, setToken] = useState(null);
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const [token, setToken] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const hash = getTokenFromUrl();
-        window.location.hash = "";
-        const _token = hash.access_token;
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
 
-        if (_token) {
-            setToken(_token);
-            spotifyApi.setAccessToken(_token);
+    if (_token) {
+      setToken(_token);
+      spotifyApi.setAccessToken(_token);
 
-            // Obtener información del usuario actual
-            spotifyApi.getMe()
-                .then((data) => {
-                    setUser(data.body);
+      // Obtener información del usuario actual
+      spotifyApi.getMe()
+        .then((data) => {
+          setUser(data.body);
+          setLoggedIn(true);
+        }, (error) => {
+          console.error('Error obtaining user information:', error);
+          setLoggedIn(false);
+        });
+    }
+  }, []);
 
-                    setUser(data.body);
-                    setLoggedIn(true);
+  const login = () => {
+    window.location.href = loginUrl;
+  };
 
-                }, (error) => {
-                    console.error('Error obtaining user information:', error);
-                    setLoggedIn(false);
-                });
-        }
-    }, []);
+  const handleCallback = () => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
 
-    const login = () => {
-        window.location.href = loginUrl;
-    };
+    if (_token) {
+      setToken(_token);
+      spotifyApi.setAccessToken(_token);
+      setLoggedIn(true);
+      navigate('/'); // Redirigir al usuario a la página principal de tu aplicación
+    }
+  };
 
-    return {
-        spotify: spotifyApi,
-        token,
-        loggedIn,
-        user,
-        login
-    };
+  return {
+    spotify: spotifyApi,
+    token,
+    loggedIn,
+    user,
+    login,
+    handleCallback
+  };
 };
