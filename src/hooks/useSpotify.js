f7d7465adfb327cfb3799183598769dba79c1ae3
MMarioB <mmariob196@gmail.com>
2025-01-08 13:47:36 +0100

    revert

--- src/hooks/useSpotify.js
+++ src/hooks/useSpotify.js
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import SpotifyWebApi from 'spotify-web-api-js';
 import { loginUrl, getTokenFromUrl } from '../lib/spotify';
 
 const spotifyApi = new SpotifyWebApi();
 
 export const useSpotify = () => {
-  const navigate = useNavigate();
   const [token, setToken] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
@@ -36,25 +34,11 @@ export const useSpotify = () => {
     window.location.href = loginUrl;
   };
 
-  const handleCallback = () => {
-    const hash = getTokenFromUrl();
-    window.location.hash = "";
-    const _token = hash.access_token;
-
-    if (_token) {
-      setToken(_token);
-      spotifyApi.setAccessToken(_token);
-      setLoggedIn(true);
-      navigate('/'); // Redirigir al usuario a la página principal de tu aplicación
-    }
-  };
-
   return {
     spotify: spotifyApi,
     token,
     loggedIn,
     user,
-    login,
-    handleCallback
+    login
   };
 };
